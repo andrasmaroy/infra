d1c5dea760e3e4d5b02b0e257bc5709199e5624f
András Maróy <andras@maroy.hu>
2021-09-23 23:37:53 +0200

    Add initial setup script

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # infra
+
+## Initial setup
+
+Fill in your SSH public key in [setup.sh](setup.sh) then copy it to the target host and run as root to setup requirements for Ansible.
---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Run as root for initial setup, all other configuration done with Ansible afterwards
+set -euo pipefail
+
+readonly SSH_PUBLIC_KEY=''
+
+apt update && apt install -y sudo python3 python3-apt
+
+/sbin/adduser --system --shell /bin/bash ansible
+/sbin/adduser ansible sudo
+echo 'ansible ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/ansible
+chmod 0440 /etc/sudoers.d/ansible
+
+mkdir /home/ansible/.ssh
+echo "${SSH_PUBLIC_KEY}" > /home/ansible/.ssh/authorized_keys
+chown -R ansible:nogroup /home/ansible/.ssh
+chmod 0600 /home/ansible/.ssh/authorized_keys
