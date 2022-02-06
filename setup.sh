629aa29321fc12922bd6c233519756d13abb2a1e
András Maróy <andras@maroy.hu>
2022-02-06 20:44:29 +0100

    Move ansible user home created by the setup script to /var

--- setup.sh
+++ setup.sh
@@ -10,12 +10,12 @@ apt update --allow-releaseinfo-change \
   && apt autoremove -y --purge \
   && apt install -y acl python3 python3-apt python3-pip sudo
 
-/usr/sbin/adduser --system --shell /bin/bash ansible
+/usr/sbin/adduser --home /var/ansible --system --shell /bin/bash ansible
 /usr/sbin/adduser ansible sudo
 echo 'ansible ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/ansible
 chmod 0440 /etc/sudoers.d/ansible
 
-mkdir /home/ansible/.ssh
-echo "${SSH_PUBLIC_KEY}" > /home/ansible/.ssh/authorized_keys
-chown -R ansible:nogroup /home/ansible/.ssh
-chmod 0600 /home/ansible/.ssh/authorized_keys
+mkdir /var/ansible/.ssh
+echo "${SSH_PUBLIC_KEY}" > /var/ansible/.ssh/authorized_keys
+chown -R ansible:nogroup /var/ansible/.ssh
+chmod 0600 /var/ansible/.ssh/authorized_keys
