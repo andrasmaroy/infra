67cdf0ff4c157fc8235982cd71b0e54ffedba00f
András Maróy <andras@maroy.hu>
2023-01-16 23:00:33 +0100

    Setup flux for deploying k8s resources
    Flux[1] is a CD tool for deploying kubernetes resources automatically to
    a cluster it is running on. Here I use the terraform provider for flux
    to do the initial bootstrapping, afterwards deploying resources can be
    done automatically by flux by adding manifests to the `kubernetes/home`
    folder.
    
    [1]: https://fluxcd.io/

---
+++ terraform/flux/github.tf
@@ -0,0 +1,58 @@
+locals {
+  known_hosts = <<-EOT
+    github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl
+    github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=
+    github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
+  EOT
+}
+
+resource "tls_private_key" "main" {
+  algorithm   = "ED25519"
+}
+
+resource "kubernetes_secret" "main" {
+  depends_on = [kubectl_manifest.install]
+
+  metadata {
+    name      = data.flux_sync.main.secret
+    namespace = data.flux_sync.main.namespace
+  }
+
+  data = {
+    identity       = tls_private_key.main.private_key_pem
+    "identity.pub" = tls_private_key.main.public_key_pem
+    known_hosts    = local.known_hosts
+  }
+}
+
+data "github_repository" "main" {
+  full_name = "${local.flux_secrets.github_owner}/${local.repository_name}"
+}
+
+resource "github_repository_deploy_key" "main" {
+  title      = "${local.cluster_name}-cluster"
+  repository = data.github_repository.main.name
+  key        = tls_private_key.main.public_key_openssh
+  read_only  = true
+}
+
+resource "github_repository_file" "install" {
+  repository = data.github_repository.main.name
+  file       = data.flux_install.main.path
+  content    = data.flux_install.main.content
+  branch     = local.branch
+}
+
+resource "github_repository_file" "sync" {
+  repository = data.github_repository.main.name
+  file       = data.flux_sync.main.path
+  content    = data.flux_sync.main.content
+  branch     = local.branch
+}
+
+resource "github_repository_file" "kustomize" {
+  repository = data.github_repository.main.name
+  file       = data.flux_sync.main.kustomize_path
+  content    = data.flux_sync.main.kustomize_content
+  branch     = local.branch
+}
---
+++ terraform/flux/sync.tf
@@ -0,0 +1,28 @@
+# Generate manifests
+data "flux_sync" "main" {
+  branch      = local.branch
+  target_path = local.target_path
+  url         = "ssh://git@github.com/${local.flux_secrets.github_owner}/${local.repository_name}"
+}
+
+# Split multi-doc YAML with
+# https://registry.terraform.io/providers/gavinbunney/kubectl/latest
+data "kubectl_file_documents" "sync" {
+  content = data.flux_sync.main.content
+}
+
+# Convert documents list to include parsed yaml data
+locals {
+  sync = [ for v in data.kubectl_file_documents.sync.documents : {
+      data: yamldecode(v)
+      content: v
+    }
+  ]
+}
+
+# Apply manifests on the cluster
+resource "kubectl_manifest" "sync" {
+  for_each   = { for v in local.sync : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
+  depends_on = [kubernetes_namespace.flux_system]
+  yaml_body = each.value
+}
