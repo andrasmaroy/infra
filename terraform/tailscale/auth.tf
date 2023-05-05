1645106a7a9452e89e571ce3487f90ba1043cc40
András Maróy <andras@maroy.hu>
2023-05-05 10:22:50 +0200

    Create tailscale API key for k3s hosted endpoint via terraform

---
+++ terraform/tailscale/auth.tf
@@ -0,0 +1,23 @@
+resource "tailscale_acl" "main" {
+  acl = jsonencode({
+    acls : [
+      {
+        // Allow all users access to all ports.
+        action = "accept",
+        users  = ["*"],
+        ports  = ["*:*"],
+    }],
+    "tagowners" : {
+      "tag:kubi" : ["autogroup:admin"]
+    }
+  })
+}
+
+resource "tailscale_tailnet_key" "kubi" {
+  reusable      = true
+  ephemeral     = true
+  preauthorized = true
+  tags = [
+    "tag:kubi"
+  ]
+}
