b32416c8c6eebd4ab32eb202a45c97d14dffb1b3
András Maróy <andras@maroy.hu>
2023-05-05 15:46:43 +0200

    Add split-dns placeholder for tailscale dns setup
    Split-dns support is not yet implemented in the terraform provider.
    https://github.com/tailscale/terraform-provider-tailscale/issues/149

---
+++ terraform/tailscale/dns.tf
@@ -0,0 +1,7 @@
+# TODO: implement when split-dns support is added
+# https://github.com/tailscale/terraform-provider-tailscale/issues/149
+# resource "tailscale_dns_nameservers" "local" {
+#   nameservers = [
+#     nonsensitive(local.secrets.metallb_address)
+#   ]
+# }
