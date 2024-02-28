fc57039b75eb92cb3e97c8e6d148db44eff1f716
András Maróy <andras@maroy.hu>
2024-02-28 11:01:54 +0100

    Update sonarr delay profile

--- terraform/media/sonarr/profiles.tf
+++ terraform/media/sonarr/profiles.tf
@@ -1,10 +1,12 @@
 resource "sonarr_delay_profile" "main" {
-  enable_usenet             = true
-  enable_torrent            = true
-  bypass_if_highest_quality = true
-  usenet_delay              = 0
-  torrent_delay             = 120
-  tags                      = []
-  preferred_protocol        = "torrent"
+  bypass_if_above_custom_format_score  = true
+  bypass_if_highest_quality            = false
+  enable_torrent                       = true
+  enable_usenet                        = true
+  minimum_custom_format_score          = 1600
+  preferred_protocol                   = "torrent"
+  tags                                 = []
+  torrent_delay                        = 120
+  usenet_delay                         = 0
 }
 
