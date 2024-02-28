7a260085563f96486a31936212cb2fb711710e93
András Maróy <andras@maroy.hu>
2024-02-28 11:32:29 +0100

    Fix tflint whitespace error

--- terraform/media/sonarr/profiles.tf
+++ terraform/media/sonarr/profiles.tf
@@ -1,12 +1,12 @@
 resource "sonarr_delay_profile" "main" {
-  bypass_if_above_custom_format_score  = true
-  bypass_if_highest_quality            = false
-  enable_torrent                       = true
-  enable_usenet                        = true
-  minimum_custom_format_score          = 1600
-  preferred_protocol                   = "torrent"
-  tags                                 = []
-  torrent_delay                        = 120
-  usenet_delay                         = 0
+  bypass_if_above_custom_format_score = true
+  bypass_if_highest_quality           = false
+  enable_torrent                      = true
+  enable_usenet                       = true
+  minimum_custom_format_score         = 1600
+  preferred_protocol                  = "torrent"
+  tags                                = []
+  torrent_delay                       = 120
+  usenet_delay                        = 0
 }
 
