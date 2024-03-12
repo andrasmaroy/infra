0d7cd50ba3a753f5fee5eebdeb8cb06b284fab62
András Maróy <andras@maroy.hu>
2024-03-12 21:19:46 +0100

    Add delay profile in sonarr for dolby vision and dolby vision + atmos content

--- terraform/media/sonarr/profiles.tf
+++ terraform/media/sonarr/profiles.tf
@@ -10,3 +10,26 @@ resource "sonarr_delay_profile" "main" {
   usenet_delay                        = 0
 }
 
+resource "sonarr_delay_profile" "DoVi" {
+  bypass_if_above_custom_format_score = true
+  bypass_if_highest_quality           = false
+  enable_torrent                      = true
+  enable_usenet                       = true
+  minimum_custom_format_score         = 1500
+  preferred_protocol                  = "torrent"
+  tags                                = [sonarr_tag.DoVi.id]
+  torrent_delay                       = 120
+  usenet_delay                        = 0
+}
+
+resource "sonarr_delay_profile" "DoViAtmos" {
+  bypass_if_above_custom_format_score = true
+  bypass_if_highest_quality           = false
+  enable_torrent                      = true
+  enable_usenet                       = true
+  minimum_custom_format_score         = 2600
+  preferred_protocol                  = "torrent"
+  tags                                = [sonarr_tag.DoViAtmos.id]
+  torrent_delay                       = 120
+  usenet_delay                        = 0
+}
